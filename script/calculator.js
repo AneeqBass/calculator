142ff943d038ddf36ca0f1eede457f27eb0e89c9
AneeqBass <aneeqbass@gmail.com>
2024-05-20 16:07:28 +0200

    added fixes and potential to a better design

--- script/calculator.js
+++ script/calculator.js
@@ -2,11 +2,12 @@ let buttons = document.getElementsByTagName('button');
 let calcString = "";
 let numbers = [0.0];
 let numString = "";
+let num1 = "";
+let num2 = "";
 let i = 0;
-let tempNum = ""
+let answer = "";
 let screen = document.querySelector('.screen')
 let equalTo = document.querySelector('#equalTo')
-
 let plus = document.querySelector('#plus')
 let minus = document.querySelector('#minus')
 let divide = document.querySelector('#divide')
@@ -16,22 +17,104 @@ let decimal = document.querySelector('#decimal')
 equalTo.style.gridColumn = "3/5" 
 
 function calculation(calcString) {
-    screen.innerText = eval(calcString)
+    return eval(calcString)
 }
+
+
 for (let button of buttons) {
     button.addEventListener('click', (event) => {
 
-        // equalTo.disabled = true;
-        //     plus.disabled = true;
-        //     minus.disabled = true;
-        //     divide.disabled = true;
-        //     multiply.disabled = true;
-        //     decimal.disabled = true;
+        if ((event.target.innerText) == "1") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "2") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "3") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "4") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "5") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "6") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "7") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "8") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "9") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
+        if ((event.target.innerText) == "0") {
+            screen.innerText == "0"?numString = event.target.innerText: numString += event.target.innerText;
+            plus.disabled = false;
+            minus.disabled = false;
+            divide.disabled = false;
+            multiply.disabled = false;
+            decimal.disabled = false;
+            equalTo.disabled = false;
+        }
 
-        // if (screen.innerText == "0") {
-        //     calcString = "";
-            
-        // }
         if (event.target.innerText == "+") {
             
             plus.disabled = true;
@@ -40,6 +123,7 @@ for (let button of buttons) {
             multiply.disabled = true;
             decimal.disabled = true;
             equalTo.disabled = true;
+            numString = "";
         }
         if (event.target.innerText == "-") {
             plus.disabled = true;
@@ -48,6 +132,7 @@ for (let button of buttons) {
             multiply.disabled = true;
             decimal.disabled = true;
             equalTo.disabled = true;
+            numString = "";
         }
         if (event.target.innerText == ".") {
             plus.disabled = true;
@@ -58,24 +143,28 @@ for (let button of buttons) {
             equalTo.disabled = true;
         }
         if (event.target.innerText == "=") {
-            calculation(calcString)
+            screen.innerText = calculation(calcString)
             equalTo.disabled = true;
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
+            plus.disabled = true;
+            minus.disabled = true;
+            divide.disabled = true;
+            multiply.disabled = true;
+            decimal.disabled = true;
             calcString = "";
+            numString = "";
+            num1 = "";
+            num2 = "";
         }
         else if (event.target.value == "/") {
             calcString += event.target.value;
-            screen.innerText = calcString;
+            screen.innerText += calcString;
             plus.disabled = true;
             minus.disabled = true;
             divide.disabled = true;
             multiply.disabled = true;
             decimal.disabled = true;
             equalTo.disabled = true;
+            numString = "";
         }
         else if (event.target.innerText == "x") {
             calcString += event.target.value;
@@ -86,8 +175,9 @@ for (let button of buttons) {
             multiply.disabled = true;
             decimal.disabled = true;
             equalTo.disabled = true;
+            numString = "";
         }
-        else if ((event.target.innerText) == "C") {
+        else if (event.target.innerText == "C") {
             calcString = "0";
             screen.innerText = calcString;
             plus.disabled = true;
@@ -96,85 +186,26 @@ for (let button of buttons) {
             multiply.disabled = true;
             decimal.disabled = true;
             equalTo.disabled = true;
+            numString = "";
+            num1 = "";
+            num2 = "";
         }
         else {
             screen.innerText == "0"?calcString = event.target.innerText: calcString += event.target.innerText;
+
             screen.innerText = calcString;
-            console.log(calcString)
         }
-         if ((event.target.innerText) == "1") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "2") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "3") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "4") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "5") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "6") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "7") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "8") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
-        }
-        if ((event.target.innerText) == "9") {
-            plus.disabled = false;
-            minus.disabled = false;
-            divide.disabled = false;
-            multiply.disabled = false;
-            decimal.disabled = false;
-            equalTo.disabled = false;
+        
+        if (num1 == "" || num2 == ""){
+            num1 = "Num1 " + numString;
+        }else {
+            num2 = "Num2 " + numString;
+            
         }
 
+        console.log(numString) 
+        console.log(num1) 
+        console.log(num2) 
     })
 }
 
